8a9044aacabbac7151feb4646305c830173159fb
leeseohyeon5 <seolee2156@gmail.com>
2025-07-30 12:49:36 +0900

    Add files via upload

---
+++ supra/script/index.js
@@ -0,0 +1,28 @@
+$(function(){ 
+    $(".searchWrap form>button").click(function(){
+        $(".searchOn").css({"display":"block"});
+        $("body").addClass("on");
+    })
+    $(".searchOn .close").click(function(e){
+        e.preventDefault();
+        $(".searchOn").css({"display":"none"});
+        $("body").removeClass("on");
+    })
+    
+    $(".navBtn").click(function(){
+        $("#nav").addClass("on");
+        $("body").addClass("on");
+    })
+    $("#nav strong>.close").click(function(e){
+        e.preventDefault();
+        $("#nav").removeClass("on");
+        $("body").removeClass("on");
+    })
+    
+    $(window).resize(function(){
+        var w=$(window).width();
+        if(w>=1024){
+            $(".close").click();
+        }
+    })
+});
